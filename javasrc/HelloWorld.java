47c63b27509f4f3ffe5da8b87681feba0ac94247
Nyasd <2841314692@qq.com>
2026-03-10 16:01:33 +0800

    ch1:Hello World

---
+++ javasrc/HelloWorld.java
@@ -0,0 +1,8 @@
+package cs2025.hfut.ch1;
+
+public class HelloWorld{
+	public static void main(String[] args){
+		int num;
+		System.out.println("Hello World");
+	}
+} 
---
+++ javasrc/MyArray.java
@@ -0,0 +1,18 @@
+package ch1;
+
+public class MyArray{
+	public static void main(String[] args){
+		int[] arr=getArray(10,30);
+		for(int i=0;i<arr.length;i++){
+			System.out.println(arr[i]);
+		}
+	}
+	static int[] getArray(int m,int n){
+		int[] result =new int[m];
+		for(int i=0;i<m;i++){
+			int tmp=(int)(Math.random()*n+1);
+			result[i]=tmp;
+		}
+		return result;
+	}
+}
